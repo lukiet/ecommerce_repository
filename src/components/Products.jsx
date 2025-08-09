ddb8fb09d46993f439d5662ed824f474a20ee4e0
lukiet <luketsalwa@gmail.com>
2025-08-09 10:21:12 +0300

    updates

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,12 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { addCart } from "../redux/action";
+import ProductCard from "./ProductCard";
 
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
-
-import { Link } from "react-router-dom";
-import toast from "react-hot-toast";
 
 const Products = () => {
   const [data, setData] = useState([]);
@@ -108,50 +104,35 @@ const Products = () => {
         </div>
 
         {filter.map((product) => {
+          // Create sample variants for demonstration
+          const variants = [];
+          
+          // Add size variants if it's clothing
+          if (product.category?.includes('clothing')) {
+            variants.push(
+              { id: 'small', name: 'Small', stock: Math.floor(Math.random() * 10) + 1 },
+              { id: 'medium', name: 'Medium', stock: Math.floor(Math.random() * 10) + 1 },
+              { id: 'large', name: 'Large', stock: Math.floor(Math.random() * 10) + 1 }
+            );
+          }
+          
+          // Add color variants for other products
+          if (product.category === 'electronics') {
+            variants.push(
+              { id: 'black', name: 'Black', stock: Math.floor(Math.random() * 10) + 1 },
+              { id: 'white', name: 'White', stock: Math.floor(Math.random() * 10) + 1 }
+            );
+          }
+
           return (
             <div
-              id={product.id}
               key={product.id}
-              className="col-md-4 col-sm-6 col-xs-8 col-12 mb-4"
+              className="col-lg-4 col-md-6 col-sm-6 col-12 mb-4"
             >
-              <div className="card text-center h-100" key={product.id}>
-                <img
-                  className="card-img-top p-3"
-                  src={product.image}
-                  alt="Card"
-                  height={300}
-                />
-                <div className="card-body">
-                  <h5 className="card-title">
-                    {product.title.substring(0, 12)}...
-                  </h5>
-                  <p className="card-text">
-                    {product.description.substring(0, 90)}...
-                  </p>
-                </div>
-                <ul className="list-group list-group-flush">
-                  <li className="list-group-item lead">$ {product.price}</li>
-                  {/* <li className="list-group-item">Dapibus ac facilisis in</li>
-                    <li className="list-group-item">Vestibulum at eros</li> */}
-                </ul>
-                <div className="card-body">
-                  <Link
-                    to={"/product/" + product.id}
-                    className="btn btn-dark m-1"
-                  >
-                    Buy Now
-                  </Link>
-                  <button
-                    className="btn btn-dark m-1"
-                    onClick={() => {
-                      toast.success("Added to cart");
-                      addProduct(product);
-                    }}
-                  >
-                    Add to Cart
-                  </button>
-                </div>
-              </div>
+              <ProductCard 
+                product={product} 
+                variants={variants}
+              />
             </div>
           );
         })}
